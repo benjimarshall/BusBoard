2be28bf07141bdfb5a378f92202d671aee518457
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-20 14:28:33 +0100

    Classes to mirror received JSON objects

---
+++ BusBoard.ConsoleApp/PredictionTiming.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusBoard.ConsoleApp
+{
+    class PredictionTiming
+    {
+        public DateTime countdownServerAdjustment { get; set; }
+        public DateTime source { get; set; }
+        public DateTime insert { get; set; }
+        public DateTime read { get; set; }
+        public DateTime sent { get; set; }
+        public DateTime received { get; set; }
+
+    }
+}
