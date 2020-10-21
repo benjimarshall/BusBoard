dff12943bd6f77c21e3fc3feb509403ed8e0e309
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-21 14:48:30 +0100

    Basic busboard website

--- BusBoard.Web/ViewModels/BusInfo.cs
+++ BusBoard.Web/ViewModels/BusInfo.cs
@@ -1,13 +1,45 @@
-﻿namespace BusBoard.Web.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BusBoard.Api.Postcode;
+using BusBoard.Api.Tfl;
+using BusBoard.Api.Tfl.Responses;
+
+namespace BusBoard.Web.ViewModels
 {
   public class BusInfo
   {
+    public IEnumerable<StopPoint> StopPoints { get; } = new List<StopPoint>();
+    public string ErrorMessage { get; } = "";
+
+    private TflApi tflApi = new TflApi();
+    private PostcodeApi postcodeApi = new PostcodeApi();
+
     public BusInfo(string postCode)
     {
-      PostCode = postCode;
+      try
+      {
+        var postcodeData = postcodeApi.GetPostcodeData(postCode);
+
+        if (postcodeData == null)
+        {
+          return;
+        }
+
+        StopPoints = tflApi.GetStopPointsAtLocation(postcodeData.latitude, postcodeData.longitude);
+        ErrorMessage = "";
+      }
+      catch (Exception ex) when (ex is TflApiException || ex is PostcodeApiException)
+      {
+        ErrorMessage = (ex.Message);
+      }
     }
 
     public string PostCode { get; set; }
 
+    public IEnumerable<Prediction> GetPredictions(StopPoint stop)
+    {
+      return tflApi.GetSortedArrivals(stop.id);
+    }
   }
 }
